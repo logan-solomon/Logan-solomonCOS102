8323224ee649b56a0d2e30c2884f4ec0c87d7741
tayyib haruna <tayyib.haruna@pau.edu.ng>
2025-03-27 22:29:42 +0100

    week_2

--- Week_1/PROJECT/SI CALC.py
+++ Week_1/PROJECT/SI CALC.py
@@ -1,9 +1,10 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('PROJECT 1')
-... print('simple interest calculator')
-... P= float(input('enter the principal value'))
-... R= float(input('enter the rate per anumn'))
-... T=float(input('enter the time'))
-... A = P*(1 + (R/100)*T)
+
+print('PROJECT 1')
+print('simple interest calculator:')
+P= float(input('enter the principal value:'))
+R= float(input('enter the rate per anumn:'))
+T=float(input('enter the time:'))
+A = P*(1 + (R/100)*T)
+print('the simple interest is:', round(A,2))
+input()
 
